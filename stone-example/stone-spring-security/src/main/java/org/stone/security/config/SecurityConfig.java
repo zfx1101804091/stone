a455407d80d973a07f19baad3f917f04b9196c2a
zhengfengxiang <1875586562@qq.com>
2021-01-22 10:11:43 +0800

    security-demo使用自定义页面

--- stone-example/stone-spring-security/src/main/java/org/stone/security/config/SecurityConfig.java
+++ stone-example/stone-spring-security/src/main/java/org/stone/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,9 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()    //自定义登录页面
-            .loginPage("login.html")
-            .loginProcessingUrl("/user/login")
-                .defaultSuccessUrl("/ts/index").permitAll()
-                .and().authorizeRequests().antMatchers();
+            .loginPage("/ts/login")
+            .loginProcessingUrl("/ts/login") //登录访问路径
+            .defaultSuccessUrl("/ts/index").permitAll() //登录成功后跳转路径
+            .and().authorizeRequests()
+            .antMatchers("/","/ts/login","/ts/t1").permitAll() //设置那些路径不需要认证
+            .anyRequest().authenticated()
+            .and().csrf().disable(); //关闭crsf防护
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //解决静态资源被拦截的问题
+        web.ignoring().antMatchers("/css/**","/templates/**");
     }
 }
--- stone-example/stone-spring-security/src/main/java/org/stone/security/controller/TestController.java
+++ stone-example/stone-spring-security/src/main/java/org/stone/security/controller/TestController.java
@@ -1,15 +1,35 @@
 package org.stone.security.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("ts")
 public class TestController {
 
     @GetMapping("/t1")
+    @ResponseBody
     public String test1(){
         return "test1";
     }
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+
 }
